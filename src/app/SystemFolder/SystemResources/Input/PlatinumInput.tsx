dad192ee32c533ed61fa859011e6106789bf3438
Robbie Byrd <me@robbiebyrd.com>
2024-03-11 23:14:33 -0500

    Allow passing down ref  in PlatinumInput (will probably need to enable this for other input items as well)

--- src/app/SystemFolder/SystemResources/Input/PlatinumInput.tsx
+++ src/app/SystemFolder/SystemResources/Input/PlatinumInput.tsx
@@ -12,36 +12,40 @@ interface PlatinumInputProps {
     prefillValue?: string;
     disabled?: boolean;
     isDefault?: boolean;
+    ref?: any;
 }
 
-const PlatinumInput: React.FC<PlatinumInputProps> = ({
-                                                         id,
-                                                         inputType = "text",
-                                                         labelTitle,
-                                                         placeholder,
-                                                         prefillValue,
-                                                         disabled = false,
-                                                         isDefault,
-                                                         onChangeFunc
-                                                     }) => {
+const PlatinumInput: React.FC<PlatinumInputProps> = React.forwardRef<HTMLInputElement, PlatinumInputProps>(
+    function PlatinumInput(
+        {
+            id,
+            inputType = "text",
+            labelTitle,
+            placeholder,
+            prefillValue,
+            disabled = false,
+            isDefault,
+            onChangeFunc
+        }, ref) {
 
-    return (<div className={platinumInputStyles.platinumInputHolder}>
-        {labelTitle &&
-            <PlatinumControlLabel label={labelTitle} labelFor={id} direction={"left"}
-                                  disabled={disabled}></PlatinumControlLabel>
-        }
-        <input id={id}
-               tabIndex={0}
-               onChange={onChangeFunc}
-               name={id}
-               type={inputType}
-               disabled={disabled}
-               value={prefillValue}
-               placeholder={placeholder}
-               className={classNames(
-                   platinumInputStyles.platinumInput, isDefault ? platinumInputStyles.platinumInputDefault : "")}
-        ></input>
-    </div>);
-};
+        return (<div className={platinumInputStyles.platinumInputHolder}>
+            {labelTitle &&
+                <PlatinumControlLabel label={labelTitle} labelFor={id} direction={"left"}
+                                      disabled={disabled}></PlatinumControlLabel>
+            }
+            <input id={id}
+                   tabIndex={0}
+                   onChange={onChangeFunc}
+                   name={id}
+                   type={inputType}
+                   ref={ref}
+                   disabled={disabled}
+                   value={prefillValue}
+                   placeholder={placeholder}
+                   className={classNames(
+                       platinumInputStyles.platinumInput, isDefault ? platinumInputStyles.platinumInputDefault : "")}
+            ></input>
+        </div>);
+    });
 
 export default PlatinumInput;
